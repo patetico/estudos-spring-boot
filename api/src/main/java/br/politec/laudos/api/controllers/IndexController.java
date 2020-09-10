d25172d043aee285f04f3a700604e0460960a9eb
Patetico <pat@pateti.com>
2020-09-10 17:03:20 -0400

    Implementa CORS para integração com o frontend

--- api/src/main/java/br/politec/laudos/api/controllers/IndexController.java
+++ api/src/main/java/br/politec/laudos/api/controllers/IndexController.java
@@ -1,7 +1,10 @@
 package br.politec.laudos.api.controllers;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @RestController
@@ -9,5 +12,17 @@ public class IndexController {
     @RequestMapping("/")
     public String index() {
         return "Hello World! Spring Boot!";
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                    // .allowedOrigins("http://localhost:3000")
+                    .allowedMethods("*");
+            }
+        };
     }
 }
